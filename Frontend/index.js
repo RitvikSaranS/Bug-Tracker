d0afdf93824c12a1ffa2ddcdea285d168fbee796
Ritvik Saran S <codebyritvik@gmail.com>
2023-07-21 14:38:04 +0530

    fetch method

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,25 +1,43 @@
 bugTemplate = `<div class="bug"><div class="bug-header"><%%></div><div class="bug-description"><%%></div><div class="assigned-to"><%%></div></div>`;
 assigneeTemplate = `<option value="<%%>"><%%></option>`;
 
-allBugsArray = [
-  {
-    bugHeader: "Missing Header",
-    bugDescription: "The header is missing in the home page",
-    assignedTo: "Shyam",
-  },
-  {
-    bugHeader: "Missing Footer",
-    bugDescription: "The footer is missing in the home page",
-    assignedTo: "Sarang",
-  },
-  {
-    bugHeader: "Missing Sidebar",
-    bugDescription: "The sidebar is missing in the home page",
-    assignedTo: "Ritvik",
-  },
-];
+// allBugsArray = [
+//   {
+//     bugHeader: "Missing Header",
+//     bugDescription: "The header is missing in the home page",
+//     assignedTo: "Shyam",
+//   },
+//   {
+//     bugHeader: "Missing Footer",
+//     bugDescription: "The footer is missing in the home page",
+//     assignedTo: "Sarang",
+//   },
+//   {
+//     bugHeader: "Missing Sidebar",
+//     bugDescription: "The sidebar is missing in the home page",
+//     assignedTo: "Ritvik",
+//   },
+// ];
 
-allAssigneeArray = ["Sarang", "Shyam", "Ritvik", "Athul"];
+// allAssigneeArray = ["Sarang", "Shyam", "Ritvik", "Athul"];
+
+function getUsers() {
+  const requestUrl = "http://localhost:3000/users";
+  fetch(requestUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
+}
+
+function getBugs() {
+  const requestUrl = "http://localhost:3000/bugs";
+  fetch(requestUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
+}
 
 function fillBugs() {
   let allBugs = "";
